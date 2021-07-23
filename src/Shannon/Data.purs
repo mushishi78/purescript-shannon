b7d03720c38a1f53d96708d3cbe0d423078b7a12
Max White <mushishi78@gmail.com>
2021-07-23 08:57:44 +0100

    Add proxy helpers for table schema

--- src/Shannon/Data.purs
+++ src/Shannon/Data.purs
@@ -5,6 +5,7 @@ import Prelude
 import Prim.RowList (Cons, Nil) as RowList
 import Prim.RowList (class RowToList, RowList)
 import Type.Function (FLIP)
+import Type.Proxy (Proxy(..))
 
 infixl 0 type FLIP as #
 
@@ -58,3 +59,23 @@ data Index_
 foreign import data Index :: Symbol -> Index_
 foreign import data CompoundIndex :: Symbol -> Index_ -> Index_
 
+
+outbound :: forall incr. Proxy incr -> Proxy (OutboundPrimaryKey incr)
+outbound _ = Proxy
+
+inbound :: forall incr indx. Proxy incr -> Proxy indx -> Proxy (InboundPrimaryKey incr indx)
+inbound _ _ = Proxy
+
+withIndex :: forall uniq indx tail. Proxy uniq -> Proxy indx -> Proxy tail -> Proxy (WithIndex uniq indx tail)
+withIndex _ _ _ = Proxy
+
+incrementing = Proxy :: Proxy Incrementing
+nonIncrementing = Proxy :: Proxy NonIncrementing
+unique = Proxy :: Proxy Unique
+notUnique = Proxy :: Proxy NotUnique
+
+index :: forall sym. Proxy sym -> Proxy (Index sym)
+index _ = Proxy
+
+compoundIndex :: forall sym indx. Proxy sym -> Proxy indx -> Proxy (CompoundIndex sym indx)
+compoundIndex _ _ = Proxy
