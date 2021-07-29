99197761194f6de7dd4bbc4de2c09634f33d040c
Max White <mushishi78@gmail.com>
2021-07-29 15:24:34 +0100

    Make Migration a newtype

--- src/Shannon/Data/Migration.purs
+++ src/Shannon/Data/Migration.purs
@@ -4,11 +4,12 @@ import Shannon.Data.DatabaseSchema (class DatabaseSchema)
 import Shannon.Data.MigrationSteps (MigrationSteps)
 import Type.Data.Peano (class IsNat, Nat)
 
-data Migration :: forall k. Nat -> Row k -> Type
-data Migration version databaseSchema = Migration (
+newtype Migration :: forall k. Nat -> Row k -> Type
+newtype Migration version databaseSchema = Migration (
   IsNat version =>
   DatabaseSchema databaseSchema =>
     { dbName :: String
     , steps :: MigrationSteps
     }
   )
+
