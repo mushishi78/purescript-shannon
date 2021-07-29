1a89798e79216a7394f9b90f1a1f4ac8b8dbd6e1
Max White <mushishi78@gmail.com>
2021-07-29 08:37:19 +0100

    Add custom error mesage for adding tables
    That tells the user that the table must not be in schema already

--- src/Shannon/Migration.purs
+++ src/Shannon/Migration.purs
@@ -7,7 +7,7 @@ import Data.NonEmpty (singleton) as NonEmpty
 import Data.Symbol (class IsSymbol, reflectSymbol)
 import Foreign.Object as Object
 import Prim.Ordering (LT)
-import Prim.Row (class Cons, class Lacks)
+import Prim.Row (class Cons)
 import Record as Record
 import Shannon.Data.DatabaseSchema (class DatabaseSchema)
 import Shannon.Data.Migration (Migration(..))
@@ -15,6 +15,7 @@ import Shannon.Data.MigrationStep as MigrationStep
 import Shannon.Data.MigrationSteps as MigrationSteps
 import Shannon.Data.TableSchema (class TableSchemaCons)
 import Shannon.Symbol (_steps_, _stores_)
+import Shannon.Type.MustNotHaveTable (class MustNotHaveTable)
 import Shannon.Type.SerializeSchema (class SerializeTableSchema, serializeTableSchema)
 import Type.Data.Peano.Nat (class CompareNat, class IsNat, D0, reflectNat)
 import Type.Proxy (Proxy(..))
@@ -33,12 +34,12 @@ newVersion _ (Migration m) = Migration $ Record.modify _steps_ (MigrationSteps.c
     newStep = MigrationStep.empty $ reflectNat $ (Proxy :: Proxy v2)
 
 addTable ::
-  forall version currentSchema tableName tableSchema mergedSchema.
+  forall tableName tableSchema version currentSchema mergedSchema.
   IsSymbol tableName =>
-  Lacks tableName currentSchema =>
   Cons tableName tableSchema currentSchema mergedSchema =>
   DatabaseSchema currentSchema =>
   SerializeTableSchema tableSchema =>
+  MustNotHaveTable tableName currentSchema =>
   Proxy tableName -> Proxy tableSchema -> Migration version currentSchema -> Migration version mergedSchema
 addTable tableName tableSchema (Migration m) = Migration $ updateSteps m
   where
---
+++ src/Shannon/Type/MustNotHaveTable.purs
@@ -0,0 +1,33 @@
+module Shannon.Type.MustNotHaveTable where
+
+import Data.Symbol (class IsSymbol)
+import Prim.RowList (class RowToList, Cons, Nil, RowList)
+import Prim.TypeError (class Fail, Beside, Text)
+
+infixl 4 type Beside as <>
+
+--
+
+class MustNotHaveTable :: forall k. Symbol -> Row k -> Constraint
+class MustNotHaveTable tableName databaseSchema
+
+instance mustNotHaveTable
+  :: (RowToList databaseSchema databaseSchemaRowList, MustNotHaveTableRowList tableName databaseSchemaRowList)
+  => MustNotHaveTable tableName databaseSchema
+
+--
+
+class MustNotHaveTableRowList :: forall k. Symbol -> RowList k -> Constraint
+class MustNotHaveTableRowList tableName databaseSchemaRowList
+
+instance mustLackRowList_Fail
+  :: ( Fail (Text "Already have table called " <> Text tableName <> Text " in database")
+     , IsSymbol tableName
+     )
+  => MustNotHaveTableRowList tableName (Cons tableName tableSchema tail)
+
+else instance mustLackRowList_Looking
+  :: ( MustNotHaveTableRowList tableName tail )
+  => MustNotHaveTableRowList tableName (Cons otherTableName otherTableSchema tail)
+
+else instance mustLackRowList_Nil :: MustNotHaveTableRowList tableName Nil
